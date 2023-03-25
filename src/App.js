c8aae0c4571420de022e0bec185640572ebf8376
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-03-25 01:01:53 -0700

    first commit which includes some cleanup and rough file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Intro from './components/Intro/Intro';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div style={{ height: '100%', width: '100%' }}>
+          <Routes>
+            <Route path="/" element={<Intro />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About/About.js
@@ -0,0 +1,19 @@
+// src/components/About.js
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <p>
+        I'm Andrew, a web developer with a passion for creating sleek,
+        retro-futuristic styled applications. I have experience in various
+        programming languages and frameworks, including React, JavaScript, and
+        more.
+      </p>
+    </div>
+  );
+};
+
+export default About;
